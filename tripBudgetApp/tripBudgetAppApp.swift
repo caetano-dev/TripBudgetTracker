d2230c50b32f954b1d365b70a8edcc5c13d33475
drull <pcaetanop@protonmail.com>
2023-11-17 20:37:34 -0300

    Initial Commit

---
+++ tripBudgetApp/tripBudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  tripBudgetAppApp.swift
+//  tripBudgetApp
+//
+//  Created by Pedro Caetano on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct tripBudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
